c0342c8082fd42367e8a53a654ab8adf455fdcf7
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-02-21 11:52:13 -0300

    Adiciona rota de conversão no arquivo web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -19,5 +18,7 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Index');
 });
+
+Route::post('/convert', [App\Http\Controllers\ConvertController::class, 'convert'])->name('convert');
 
 require __DIR__.'/auth.php';
